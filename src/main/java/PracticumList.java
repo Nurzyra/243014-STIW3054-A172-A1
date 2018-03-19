ec04fead5b5b227b72c24c579b91e5b20c1e1e37
Nurzyra <33803299+Nurzyra@users.noreply.github.com>
2018-03-19 18:29:04 +0800

    Add files via upload

---
+++ src/main/java/PracticumList.java
@@ -0,0 +1,75 @@
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.Writer;
+
+import java.util.Iterator;
+
+public class PracticumList {
+    
+     private static final String FILE_NAME = "F:\\UUM\\SEM 5\\RealTimeProgramming\\Assignment 1\\PracticumList.xlsx";
+     public static void main(String[] args) {
+        
+        Writer writer = null;
+        boolean line = true;
+        
+        try {
+            
+            DataFormatter dataf = new DataFormatter();
+
+            FileInputStream excelFile = new FileInputStream(new File(FILE_NAME));
+            Workbook workbook = new XSSFWorkbook(excelFile);
+            Sheet datatypeSheet = workbook.getSheetAt(0);
+            Iterator<Row> iterator = datatypeSheet.iterator();
+            
+            File file = new File("F:\\UUM\\SEM 5\\RealTimeProgramming\\Assignment 1\\243014.md");
+            writer = new BufferedWriter(new FileWriter(file));
+               
+            while (iterator.hasNext()) {
+
+                Row currentRow = iterator.next();
+                Iterator<Cell> cellIterator = currentRow.iterator();
+
+                while (cellIterator.hasNext()) {
+
+                    Cell currentCell = cellIterator.next();
+                    String val = dataf.formatCellValue(currentCell);
+                    
+                    System.out.print(val +"|");
+                  
+                    
+                    writer.write(val +"|"); 
+
+                }
+                System.out.println();
+                writer.write("\n");
+                if(line==true){
+                    writer.write("---|-----|-----|-----|\n");
+                    line=false;
+                }
+                
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        try {                 
+            if (writer != null) {                     
+                writer.close();                 
+            }             
+        } catch (IOException e) {                 
+            e.printStackTrace();             
+        }         
+        
+    }
+
+}
+
